748b17d8b658f4d36f0fef6d357e0018dd25aaf8
ScooloV <scoolov@mail.ru>
2023-06-06 16:16:21 +0200

    Generating empty pages if pages count is not divisible by 4

--- static/pdf_convert.js
+++ static/pdf_convert.js
@@ -9,26 +9,22 @@ async function rearrangePDF() {
 
         const pdfDoc = await PDFLib.PDFDocument.load(typedArray);
         const totalPages = pdfDoc.getPageCount();
+        const totalPages_4 = Math.ceil(totalPages / 4) * 4
 
         let pageOrder = [];
 
-        for (let i = 1; i <= totalPages; i += 3) {
-            if (i + 2 <= totalPages) {
-                pageOrder.push(i);
-                pageOrder.push(i + 2);
-                pageOrder.push(i + 1);
-            } else {
-                for (let j = i; j <= totalPages; j++) {
-                    pageOrder.push(j);
-                }
-            }
+        for (let i = 1; i <= totalPages_4; i += 4) {
+            pageOrder.push(i);
+            pageOrder.push(i + 2);
+            pageOrder.push(i + 1);
+            pageOrder.push(i + 3);
         }
 
         const mergedPdfData = await mergePDFs(pdfDoc, pageOrder);
 
         const blob = new Blob([mergedPdfData], {type: 'application/pdf'});
         const downloadLink = document.getElementById('downloadLink');
-        const fileName = file.name.replace(/(\.pdf)$/i, '_converter$1');
+        const fileName = file.name.replace(/(\.pdf)$/i, '_converted$1');
         downloadLink.href = URL.createObjectURL(blob);
         downloadLink.download = fileName;
         document.getElementById('downloadLinkContainer').style.display = 'block';
@@ -42,8 +38,14 @@ async function mergePDFs(pdfDoc, pageOrder) {
 
     for (let i = 0; i < pageOrder.length; i++) {
         const pageNum = pageOrder[i];
-        const [copiedPage] = await mergedPdf.copyPages(pdfDoc, [pageNum - 1]);
-        mergedPdf.addPage(copiedPage);
+        if (pageNum !== null && pageNum <= pdfDoc.getPageCount()) {
+            const [copiedPage] = await mergedPdf.copyPages(pdfDoc, [pageNum - 1]);
+            mergedPdf.addPage(copiedPage);
+        } else {
+            const firstPage = pdfDoc.getPages()[0];
+            const {width, height} = firstPage.getSize();
+            const blankPage = mergedPdf.addPage([width, height]);
+        }
     }
 
     const mergedPdfBytes = await mergedPdf.save();
